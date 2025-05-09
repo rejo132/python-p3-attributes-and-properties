f2235dc6acb450e8a4b0c93c99314a8570931c0a
rejo132 <enoch.luke1@student.moringaschool.com>
2025-05-09 21:24:57 +0300

    completes lab

--- lib/dog.py
+++ lib/dog.py
@@ -12,4 +12,18 @@ APPROVED_BREEDS = [
 ]
 
 class Dog:
-    pass
+    def __init__(self, name="Beagle", breed="Beagle"):
+        if not isinstance(name, str) or len(name.strip()) == 0 or len(name) > 25:
+            print("Name must be string between 1 and 25 characters.")
+            self.name = "Beagle"
+        else:
+            self.name = name
+        
+        if breed not in APPROVED_BREEDS:
+            print("Breed must be in list of approved breeds.")
+            self.breed = "Beagle"
+        else:
+            self.breed = breed
+    
+    def sit(self):
+        print("The dog is sitting.")
--- lib/person.py
+++ lib/person.py
@@ -1,3 +1,4 @@
+
 #!/usr/bin/env python3
 
 APPROVED_JOBS = [
@@ -16,4 +17,21 @@ APPROVED_JOBS = [
 ]
 
 class Person:
-    pass
+    def __init__(self, name="Person", job="Sales"):
+        if not isinstance(name, str) or len(name.strip()) == 0 or len(name) > 25:
+            print("Name must be string between 1 and 25 characters.")
+            self.name = "Person"
+        else:
+            self.name = name.title()
+        
+        if job not in APPROVED_JOBS:
+            print("Job must be in list of approved jobs.")
+            self.job = "Sales"
+        else:
+            self.job = job
+    
+    def talk(self):
+        print("Hello World!")
+    
+    def walk(self):
+        print("The person is walking.")
